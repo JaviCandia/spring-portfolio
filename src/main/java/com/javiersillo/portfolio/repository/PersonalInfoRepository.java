4ffc16de212ef5e0180e262f652cfcc2e8f334ce
Javier Candia <javi-candia@hotmail.com>
2025-12-02 23:07:21 -0600

    Personal Info Repository Done

---
+++ src/main/java/com/javiersillo/portfolio/repository/PersonalInfoRepository.java
@@ -0,0 +1,13 @@
+package com.javiersillo.portfolio.repository;
+
+import com.javiersillo.portfolio.model.PersonalInfo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonalInfoRepository {
+    PersonalInfo save(PersonalInfo personalInfo);
+    Optional<PersonalInfo> findById(Long id);
+    List<PersonalInfo> findAll();
+    void deleteById(Long id);
+}
